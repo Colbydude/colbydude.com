7a5f574aa1498faa53c4fbfb14ef57e5b6b2031a
Colby Terry <colbydude@voidteam.net>
2014-08-18 19:25:22 -0400

    Add responsive design and minify CSS

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,17 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var minifyCss = require('gulp-minify-css');
 
 gulp.task('css', function()
 {
 	gulp.src('app/assets/sass/icons.scss')
 		.pipe(sass())
 		.pipe(autoprefixer('last 10 version'))
+		.pipe(minifyCss({keepBreaks:false}))
 		.pipe(gulp.dest('css'));
 
 	gulp.src('app/assets/sass/style.scss')
 		.pipe(sass())
 		.pipe(autoprefixer('last 10 version'))
+		.pipe(minifyCss({keepBreaks:true}))
 		.pipe(gulp.dest('css'));
 });
 
