559ec0206c8e91787f7bdc59747f48cdccdc6064
Colbydude <colbydude@gmail.com>
2015-02-25 16:57:38 -0500

    Add titles to color theme switchers.

--- app/views/layouts/partials/style-chooser.blade.php
+++ app/views/layouts/partials/style-chooser.blade.php
@@ -1,4 +1,4 @@
 <div class="style-chooser">
-	<a class="choose-dark" href="#" data-style="dark"></a>
-	<a class="choose-light" href="#" data-style="light"></a>
+	<a class="choose-dark" href="#" title="Switch to Dark Theme" data-style="dark"></a>
+	<a class="choose-light" href="#" title="Switch to Light Theme" data-style="light"></a>
 </div>
