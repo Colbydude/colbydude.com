96178e44dc68fa6890a711db2a153a669b411483
Colby Terry <colbydude@gmail.com>
2016-11-29 11:37:31 -0800

    Clear Repository.

--- resources/views/layouts/partials/errors.blade.php
+++
@@ -1,8 +0,0 @@
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-        @foreach ($errors->all() as $error)
-            <p>{{ $error }}</p>
-        @endforeach
-    </div>
-@endif
