0dfeffbee342c564959086a77253fe5c9bb2a4b5
Colby Terry <colbydude@voidteam.net>
2019-07-18 01:01:00 -0700

    Add Cron to fetch Spotify artist info. Music page redesign WIP.

--- resources/views/pages/music.blade.php
+++ resources/views/pages/music.blade.php
@@ -11,144 +11,82 @@
             <h1>Music</h1>
             <h2>Punk Rocker with a hint of metal tendencies.</h2>
         </div>
-        <h3 class="line-header"><span>Releases</span></h3>
         <div class="row">
-            <div class="col-sm-4">
-                <p><img class="img-responsive img-thumbnail" src="/img/music/releases/vgm-vol-1.jpg" alt="VGM, Vol. 1: New Game - EP" width="1400" height="1400"></p>
+            <div class="col-xs-6 col-sm-7">
+                <h3 class="line-header"><span>Popular</span></h3>
+                <ol class="tracklist">
+                    @foreach ($topTracks as $track)
+                        <li class="tracklist-row">
+                            <div class="tracklist-col tracklist-col-album-art">
+                                <div class="album-art tracklist-middle-align album-art--with-auto-height">
+                                    <div class="album-art-image" style="background-image: url('{{ $track->album->images[2]->url }}');"></div>
+                                </div>
+                            </div>
+                            <div class="tracklist-col name">
+                                <div class="track-name-wrapper tracklist-middle-align">
+                                    <div class="tracklist-name ellipsis-one-line">
+                                        {{ $track->name }}
+                                    </div>
+                                </div>
+                            </div>
+                            {{-- $track->preview_url --}}
+                            {{-- $track->duration_ms --}}
+                            {{-- $track->external_urls->spotify --}}
+                        </li>
+                    @endforeach
+                </ol>
             </div>
-            <div class="col-sm-8">
-                <h3 class="featured">VGM, Vol. 1: New Game - EP</h3>
-                <h4>Released January 2nd, 2017</h4>
-                <p>on <a href="https://voidte.am/VGMVol1-Am">Amazon</a>, <a href="https://voidte.am/VGMVol1-GP">Google Play</a>, <a href="https://voidte.am/VGMVol1-iT">iTunes</a>, <a href="https://voidte.am/VGMVol1-Sp">Spotify</a>, and <a href="https://voidte.am/VGMVol1-YT">YouTube</a></p>
-                <table class="table table-condensed text-left">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>SONG</th>
-                            <th class="text-right"><span class="far fa-clock"></span></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Final Rush (From "Sonic Adventure 2")</td>
-                            <td class="text-right">2:45</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>Stone Tower Temple (From "The Legend of Zelda: Majora's Mask")</td>
-                            <td class="text-right">3:20</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>The Man With the Machine Gun (From "Final Fantasy VIII")</td>
-                            <td class="text-right">2:59</td>
-                        </tr>
-                        <tr>
-                            <td>4</td>
-                            <td>Bowser's Road (From "Super Mario 64")</td>
-                            <td class="text-right">3:38</td>
-                        </tr>
-                        <tr>
-                            <td>5</td>
-                            <td>Force Your Way (From "Final Fantasy VIII")</td>
-                            <td class="text-right">4:16</td>
-                        </tr>
-                        <tr>
-                            <td>6</td>
-                            <td>Mute City feat. SwigglesRP (From "F-Zero")</td>
-                            <td class="text-right">2:43</td>
-                        </tr>
-                    </tbody>
-                </table>
+            <div class="col-xs-6 col-sm-5">
+                <h3 class="line-header"><span>Latest Release</span></h3>
             </div>
         </div>
-        <h3 class="line-header"><span>Axes</span></h3>
-        <div class="row">
-            <div class="col-sm-6">
-                <img class="img-responsive img-thumbnail" src="/img/guitars/ibanez-s5ex1-tg.jpg" alt="Ibanez S5EX1 Tiger Gray" width="600" height="202">
-                <h4>Ibanez S5EX1 Tiger Gray</h4>
-            </div>
-            <div class="col-sm-6">
-                <img class="img-responsive img-thumbnail" src="/img/guitars/esp-ltd-ec-1000-stb.jpg" alt="ESP LTD EC-1000 See Thru Blue" width="600" height="202">
-                <h4>ESP LTD EC-1000 See Thru Blue</h4>
-            </div>
+        <h3 class="line-header"><span>Albums</span></h3>
+        <div class="row flex-row album-list">
+            @foreach ($albums as $album)
+                <div class="col-xs-6 col-sm-4 col-md-3">
+                    <div class="media-object">
+                        <img class="img-responsive img-thumbnail" src="{{ $album->images[0]->url }}" alt="{{ $album->name }}" width="640" height="640">
+                        <div class="mo-info">
+                            <a href="#">{{ $album->name }}</a>
+                        </div>
+                        <div class="mo-meta ellipsis-one-line">
+                            <span class="text-muted">{{ $album->artists[0]->name }} &middot; {{ $album->release_date }}</span>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
+        <h3 class="line-header"><span>Singles and EPs</span></h3>
+        <h3 class="line-header"><span>Guitars</span></h3>
         <div class="row">
             <div class="col-sm-6">
-                <img class="img-responsive img-thumbnail" src="/img/guitars/chapman-ml1-8rs.jpg" alt="Chapman ML1-8RS (Rob Scallon 8-String Signature)" width="600" height="202">
-                <h4>Chapman ML1-8RS (Rob Scallon 8-String Signature)</h4>
-            </div>
-            <div class="col-sm-6">
-                <img class="img-responsive img-thumbnail" src="/img/guitars/prs-se-mhhb2.jpg" alt="PRS SE MHHB2 Holcomb Burst (Mark Holcomb Signature)" width="600" height="202">
+                <img class="img-responsive img-thumbnail" src="/img/music/guitars/prs-se-mhhb2.jpg" alt="PRS SE MHHB2 Holcomb Burst (Mark Holcomb Signature)" width="600" height="202">
                 <h4>PRS SE MHHB2 Holcomb Burst (Mark Holcomb Signature)</h4>
             </div>
+            <div class="col-sm-6">
+                <img class="img-responsive img-thumbnail" src="/img/music/guitars/ibanez-s61al-bm.jpg" alt="Ibanez S61AL Black Mirage" width="600" height="202">
+                <h4>Ibanez S61AL Black Mirage</h4>
+            </div>
         </div>
         <div class="row">
             <div class="col-sm-6">
-                <img class="img-resonsive img-thumbnail" src="/img/guitars/esp-sn-25th.jpg" alt="ESP SN-25TH (Sonic the Hedgehog)">
+                <img class="img-resonsive img-thumbnail" src="/img/music/guitars/esp-sn-25th.jpg" alt="ESP SN-25TH (Sonic the Hedgehog)">
                 <h4>ESP SN-25TH (Sonic the Hedgehog)</h4>
             </div>
             <div class="col-sm-6">
-                <img class="img-responsive img-thumbnail" src="/img/guitars/esp-sd-15th.jpg" alt="ESP SD-15TH (Shadow the Hedgehog)">
+                <img class="img-responsive img-thumbnail" src="/img/music/guitars/esp-sd-15th.jpg" alt="ESP SD-15TH (Shadow the Hedgehog)">
                 <h4>ESP SD-15TH (Shadow the Hedgehog)</h4>
             </div>
         </div>
         <div class="row">
             <div class="col-sm-6">
-                <img class="img-responsive img-thumbnail" src="/img/guitars/esp-ltd-b-155dx-stb.jpg" alt="ESP LTD B-155DX See Thru Blue" width="600" height="202">
+                <img class="img-responsive img-thumbnail" src="/img/music/guitars/chapman-ml1-8rs.jpg" alt="Chapman ML1-8RS (Rob Scallon 8-String Signature)" width="600" height="202">
+                <h4>Chapman ML1-8RS (Rob Scallon 8-String Signature)</h4>
+            </div>
+            <div class="col-sm-6">
+                <img class="img-responsive img-thumbnail" src="/img/music/guitars/esp-ltd-b-155dx-stb.jpg" alt="ESP LTD B-155DX See Thru Blue" width="600" height="202">
                 <h4>ESP LTD B-155DX See Thru Blue</h4>
             </div>
         </div>
-        @if (isset($music))
-            <h3 class="line-header"><span>Video Game Metal</span></h3>
-            <div class="row">
-                @if (!empty($music['vg-metal']))
-                    @foreach ($music['vg-metal'] as $song)
-                        <div class="col-sm-6 col-md-4">
-                            <div class="video-container"><iframe width="635" height="353" src="{{ $song['video'] }}?wmode=transparent"></iframe></div>
-                            <h4>{{ $song['song'] }}</h4>
-                            <p>From: {{ $song['from'] }}</p>
-                        </div>
-                    @endforeach
-                @else
-                    <div class="col-xs-12">
-                        <p class="lead">Nothing to see here yet!</p>
-                    </div>
-                @endif
-            </div>
-            <h3 class="line-header"><a href="http://wedbeofficial.com" target="_blank"><span>We'd Be [My Band]</span></a></h3>
-            <div class="row">
-                @if (!empty($music['wed-be']))
-                    @foreach ($music['wed-be'] as $song)
-                        <div class="col-sm-6 col-md-4">
-                            <div class="video-container"><iframe width="635" height="353" src="{{ $song['video'] }}?wmode=transparent"></iframe></div>
-                            <h4>{{ $song['song'] }}</h4>
-                            <p>{{ $song['subtitle'] }}</p>
-                        </div>
-                    @endforeach
-                @else
-                    <div class="col-xs-12">
-                        <p class="lead">Nothing to see here yet!</p>
-                    </div>
-                @endif
-            </div>
-            <h3 class="line-header"><span>(Old) Guitar Covers</span></h3>
-            <div class="row">
-                @if (!empty($music['guitar-covers']))
-                    @foreach ($music['guitar-covers'] as $song)
-                        <div class="col-sm-6 col-md-4">
-                            <div class="video-container"><iframe width="635" height="353" src="{{ $song['video'] }}?wmode=transparent"></iframe></div>
-                            <h4>{{ $song['song'] }}</h4>
-                            <p>By: {{ $song['by'] }}</p>
-                        </div>
-                    @endforeach
-                @else
-                    <div class="col-xs-12">
-                        <p class="lead">Nothing to see here yet!</p>
-                    </div>
-                @endif
-            </div>
-        @endif
     </div>
 @stop
