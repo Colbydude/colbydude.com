19df717e5bf5077533d23ded34de2a17b332bf53
Colby Terry <colbydude@voidteam.net>
2019-07-25 11:10:42 -0700

    Combine web and games into one page. Code page WIP.

---
+++ resources/views/pages/code.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.default')
+
+@section('meta_title', 'Code | Colby Terry')
+@section('meta_description', '')
+@section('meta_keywords', 'Colby Terry, Colbydude, developer, code, gamedev, games, webdev, web, webapp, portfolio')
+@section('meta_canonical', 'https://colbydude.com/code')
+
+@section('content')
+    <div class="container">
+        <div class="page-header">
+            <h1>Code</h1>
+            <h2></h2>
+        </div>
+        <h3 class="line-header"><span>Showcase</span></h3>
+        <ul class="story-grid">
+            @foreach ($showcase as $story)
+                <li class="story">
+                    <div class="story-image">
+                        <a href="{{ $story['links'][0]['url'] }}" target="_blank">
+                            <img class="img-responsive" src="/img/code/showcase/{{ $story['image'] }}" alt="{{ $story['name'] }}">
+                        </a>
+                    </div>
+                    <div class="story-content">
+                        <h4><a href="{{ $story['links'][0]['url'] }}" target="_blank">{{ $story['name'] }}</a></h4>
+                        <p>{!! $story['description'] !!}</p>
+                        <div class="story-links">
+                            @foreach ($story['links'] as $link)
+                                <a href="{{ $link['url'] }}" target="_blank">{{ $link['text'] }}</a>
+                            @endforeach
+                        </div>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+@stop
