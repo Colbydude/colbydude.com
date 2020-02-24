6ed7fcd38b9b4a59963526e37d4a228c56fb256a
Colby Terry <colby.terry@slalom.com>
2020-02-23 22:48:55 -0800

    Update FB link.

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -5,7 +5,7 @@
                 <ul class="social-list">
                     <li><a class="link-primary" href="#" data-target="#contact-modal" data-toggle="modal" title="Contact"><span class="fa fa-envelope"></span></a></li>
                     <li><a class="link-discord" href="https://voidte.am/ColbDiscord" target="_blank" title="Discord"><span class="fab fa-discord"></span></a></li>
-                    <li><a class="link-facebook" href="https://facebook.com/ColbydudeMusic" target="_blank" title="Facebook"><span class="fab fa-facebook"></span></a></li>
+                    <li><a class="link-facebook" href="https://facebook.com/Colbydude" target="_blank" title="Facebook"><span class="fab fa-facebook"></span></a></li>
                     <li><a class="link-github" href="https://github.com/Colbydude" target="_blank" title="GitHub"><span class="fab fa-github"></span></a></li>
                     <li><a class="link-linkedin" href="https://www.linkedin.com/in/colbydude/" target="_blank" title="LinkedIn"><span class="fab fa-linkedin"></span></a></li>
                     <li><a class="link-twitch" href="http://twitch.tv/Colbydude" target="_blank" title="Twitch"><span class="fab fa-twitch"></span></a></li>
