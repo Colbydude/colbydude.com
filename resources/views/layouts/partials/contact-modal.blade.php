6aabad35fb34bc169fde5cdfe72044f8043fda2d
Colby Terry <colbydude@voidteam.net>
2022-04-21 22:59:57 -0700

    Fix label for contact modal comments.

--- resources/views/layouts/partials/contact-modal.blade.php
+++ resources/views/layouts/partials/contact-modal.blade.php
@@ -50,7 +50,7 @@
                         </div>
                     </div>
                     <div>
-                        <label for="name" class="block text-sm font-medium text-slate-800 dark:text-slate-300">Comments</label>
+                        <label for="comments" class="block text-sm font-medium text-slate-800 dark:text-slate-300">Comments</label>
                         <textarea name="comments" id="comments" class="sm:h-[190px] shadow-sm focus:ring-pink-500 focus:border-pink-500 block w-full sm:text-sm border-slate-200 dark:border-slate-700 bg-white dark:bg-slate-900 transition duration-150 ease-in-out" placeholder="Message..." required></textarea>
                     </div>
                 </div>
