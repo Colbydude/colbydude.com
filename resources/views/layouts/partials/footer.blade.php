8afaafaf9381fd963f14e59f79cc802eac0d5841
Colby Terry <colbydude@voidteam.net>
2023-01-09 15:17:26 -0500

    Remove Facebook link. Update dependencies.

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,7 +3,6 @@
         <span class="block text-sm text-center sm:text-left"><strong>Copyright &copy; {{ date('Y') }} Colby Terry.</strong></span>
         <ul class="flex justify-center items-center pt-1 pb-2 list-none sm:py-0" role="navigation" aria-label="Social">
             <li class="pr-3"><a class="text-slate-600 dark:text-slate-400 hover:text-pink-500 dark:hover:text-pink-400" href="#" data-dialog="contact" title="Contact">@svg('fontawesome/envelope', 'w-5 h-5 sm:w-6 sm:h-6')</a></li>
-            <li class="px-3"><x-social-link class="hover:text-[#1876f2] dark:hover:text-[#1876f2]" href="https://facebook.com/Colbydude" title="Facebook" svg="fontawesome/facebook" /></li>
             <li class="px-3"><x-social-link class="hover:text-[#6e5494] dark:hover:text-[#6e5494]" href="https://github.com/Colbydude" title="GitHub" svg="fontawesome/github" /></li>
             <li class="px-3"><x-social-link class="hover:text-[#0a66c2] dark:hover:text-[#0a66c2]" href="https://www.linkedin.com/in/colbydude/" title="LinkedIn" svg="fontawesome/linkedin" /></li>
             <li class="px-3"><x-social-link class="hover:text-[#9146ff] dark:hover:text-[#9146ff]" href="http://twitch.tv/Colbydude" title="Twitch" svg="fontawesome/twitch" /></li>
