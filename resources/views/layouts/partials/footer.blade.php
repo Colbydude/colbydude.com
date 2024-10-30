36ac34654be295968ceb19c6b5ba9c16dad9829f
Colby Terry <colbydude@voidteam.net>
2024-10-29 20:24:04 -0700

    Goodbye, Twitter. Hello, Bluesky.

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -3,10 +3,10 @@
         <span class="block text-sm text-center sm:text-left"><strong>Copyright &copy; {{ date('Y') }} Colby Terry.</strong></span>
         <ul class="flex justify-center items-center pt-1 pb-2 list-none sm:py-0" role="navigation" aria-label="Social">
             <li class="pr-3"><a class="text-slate-600 dark:text-slate-400 hover:text-pink-500 dark:hover:text-pink-400" href="#" data-dialog="contact" title="Contact">@svg('fontawesome/envelope', 'w-5 h-5 sm:w-6 sm:h-6')</a></li>
+            <li class="px-3"><x-social-link class="hover:text-[#0060df] dark:hover:text-[#0060df]" href="https://bsky.app/profile/colbydude.com" title="Bluesky" svg="fontawesome/bluesky" /></li>
             <li class="px-3"><x-social-link class="hover:text-[#6e5494] dark:hover:text-[#6e5494]" href="https://github.com/Colbydude" title="GitHub" svg="fontawesome/github" /></li>
             <li class="px-3"><x-social-link class="hover:text-[#0a66c2] dark:hover:text-[#0a66c2]" href="https://www.linkedin.com/in/colbydude/" title="LinkedIn" svg="fontawesome/linkedin" /></li>
             <li class="px-3"><x-social-link class="hover:text-[#9146ff] dark:hover:text-[#9146ff]" href="http://twitch.tv/Colbydude" title="Twitch" svg="fontawesome/twitch" /></li>
-            <li class="px-3"><x-social-link class="hover:text-[#1d9bf0] dark:hover:text-[#1d9bf0]" href="https://twitter.com/Colbydude" title="Twitter" svg="fontawesome/twitter" /></li>
             <li class="pl-3"><x-social-link class="hover:text-[#ff0000] dark:hover:text-[#ff0000]" href="https://www.youtube.com/channel/UCW0IHL6zJWMVjv1ufZS1sCw" title="YouTube" svg="fontawesome/youtube" /></li>
         </ul>
     </div>
